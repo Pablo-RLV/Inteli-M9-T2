950ea884dc6081d203d92fce129e5bc1c9d26760
Pablo Ruan <pablo.viana@sou.inteli.edu.br>
2024-02-19 09:36:32 -0300

    refactor(message): :art: add abstraction to sensor data

--- Ponderada1/publisher/main.go
+++ Ponderada1/publisher/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"strconv"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
@@ -22,9 +23,9 @@ func main() {
 	for {
 		data := map[string]interface{}{
 			"ID": id,
-			"CO_ppm": rand.Intn(1000),
-			"NO2_ppm": rand.Intn(10),
-			"NH3_ppm": rand.Intn(500),
+			"CO": strconv.Itoa(rand.Intn(1000)) + " ppm",
+			"NO2": strconv.Itoa(rand.Intn(10)) + " ppm",
+			"NH3": strconv.Itoa(rand.Intn(500)) + " ppm",
 		}
 
 		jsonData, err := json.Marshal(data)
